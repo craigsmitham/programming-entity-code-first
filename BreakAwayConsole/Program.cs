4a772cf3a2c91ae959863e1a5114fc7185511b68
Craig Smitham <craigsmitham@gmail.com>
2012-01-27 15:56:09 -0600

    testing default initializer

--- BreakAwayConsole/Program.cs
+++ BreakAwayConsole/Program.cs
@@ -13,8 +13,8 @@ namespace BreakAwayConsole
     {
         static void Main(string[] args)
         {
-            Database.SetInitializer(
-                new DropCreateDatabaseIfModelChanges<BreakAwayContext>());
+            //Database.SetInitializer(
+            //    new DropCreateDatabaseIfModelChanges<BreakAwayContext>());
 
             // Database.DefaultConnectionFactory = new CustomConnectionFactory();
 
